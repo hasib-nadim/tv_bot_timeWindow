70e3b02e24d215164197753a1ff98ec4ad7c2db3
hasibnadim <nadimshah206@gmail.com>
2024-10-14 17:52:33 +0600

    cluster fork implimented

--- index.js
+++ index.js
@@ -1,64 +1,11 @@
-const express = require("express");
-const { createDB, insert,getAll, getLast } = require("./query");
-const app = express();
-const port = 3000;
-app.use(express.urlencoded({ extended: true }), express.json());
-app.set("view engine", "ejs");
-
-api = express.Router();
-app.use("/api", api);
-
-//
-app.get("/", async (req, res) => {
-  try {
-    const data = await getAll();
-    res.render("index", { data: data });
-  } catch (error) {
-    console.log(error);
-    res.render("index", { data: [] });
-  }
-});
-
-api.post("/", (req, res) => {
-  const txt = req.body || "";
-  insert(txt);
-  res.end();
-});
-api.get("/send", (req, res) => {
-  const txt = req.query?.txt || "";
-  insert(txt);
-  res.end();
-});
-
-api.get("/list",async (req, res) => {
-  try {
-    const data = await last5();
-    let _ = data.map((d) => (`${Date.parse(d.created)}+${d.text}`));
-    res.json(_);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
-});
-api.get("/view",async (req, res) => {
-  try {
-    const data = await getLast();
-    res.send(`${Date.parse(data.created)}+${data.text}`);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
-});
-api.get("/docs", (req, res) => {
-  res.send(`
-    GET: / - get last 5 alerts as html <br>
-    GET: /api/list - get last 5 alerts as json <br>
-    GET: /api/view - get last alert as json <br>
-    GET: /api/send?txt="SOMETHING" - send custom alert <br>
-    POST: /api - send TV alert <br>
-    `)
-})
-app.listen(port, () => {
-  createDB();
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+const cluster = require("cluster");
+if (cluster.isPrimary) {
+  cluster.fork();
+  cluster.on("exit", (worker) => {
+    // prettier-ignore
+    console.log("\x1b[31m",`  worker ${worker.process.pid} died`,"\x1b[0m");
+    cluster.fork();
+  });
+} else {
+   require("./main");
+}
---
+++ main.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const { createDB, insert,getAll, getLast,last5 } = require("./query");
+const app = express();
+const port = 3000;
+app.use(express.text(),express.urlencoded({ extended: true }), express.json());
+app.set("view engine", "ejs");
+
+api = express.Router();
+app.use("/api", api);
+
+//
+app.get("/", async (req, res) => {
+  try {
+    const data = await getAll();
+    res.render("index", { data: data });
+  } catch (error) {
+    console.log(error);
+    res.render("index", { data: [] });
+  }
+});
+
+api.post("/", (req, res) => {
+  const txt = req.body || "";
+  insert(txt);
+  res.end();
+});
+api.get("/send", (req, res) => {
+  const txt = req.query?.txt || "";
+  insert(txt);
+  res.end();
+});
+
+api.get("/list",async (req, res) => {
+  try {
+    const data = await last5();
+    let _ = data.map((d) => (`${Date.parse(d.created)}+${d.text}`));
+    res.json(_);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+api.get("/view",async (req, res) => {
+  try {
+    const data = await getLast();
+    res.send(`${Date.parse(data.created)}+${data.text}`);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+api.get("/docs", (req, res) => {
+  res.send(`
+    GET: / - get last 5 alerts as html <br>
+    GET: /api/list - get last 5 alerts as json <br>
+    GET: /api/view - get last alert as json <br>
+    GET: /api/send?txt="SOMETHING" - send custom alert <br>
+    POST: /api - send TV alert <br>
+    `)
+})
+app.get("/opps",(req,res)=>{
+   setTimeout(()=>{
+    throw 'q'
+   },500)
+   res.send('ops')
+})
+app.listen(port, () => {
+  createDB();
+  console.log(`Example app listening at http://localhost:${port}`);
+});
