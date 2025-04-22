fec007337e9a928a201615cb8da957a25a7e5b36
hasibnadim <nadimshah206@gmail.com>
2025-04-22 21:06:33 +0600

    UI

--- index.js
+++ index.js
@@ -1,12 +1,123 @@
 const express = require("express");
-const { createDB, insert, getAll, last5, getLast } = require("./query");
+const path = require("path");
+var jwt = require("jsonwebtoken");
+var cookieParser = require("cookie-parser");
+const {
+  createDB,
+  insert,
+  getAll,
+  last5,
+  getLast,
+  findAdmin,
+  getUsers,
+  expiredKeys,
+  addNewUser,
+  extendKeyDate,
+  getUser,
+  hasAccessKey,
+} = require("./query");
+const { hashSync } = require("bcrypt");
+const { randomUUID } = require("crypto");
 const app = express();
 const port = 80;
 app.use(express.text(), express.urlencoded({ extended: true }), express.json());
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(cookieParser());
 
 api = express.Router();
+const jwtSecret = "secretNOTTOBEEXPOSED";
+
+function checkAuth(req, res, next) {
+  const token = req.cookies.authorization;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+      if (err) {
+        res.redirect("/login");
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+}
+
+app.get("/", checkAuth, async (req, res) => {
+  const userList = await getUsers();
+  const _expiredKeys = await expiredKeys();
+  res.render("admin/dashboard", { userList, expiredKeys: _expiredKeys });
+});
+
+app.get("/login", (req, res) => {
+  res.render("admin/login");
+});
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await findAdmin(email, password);
+  if (user) {
+    const token = jwt.sign({ id: user.id }, jwtSecret, { expiresIn: "1h" });
+    res
+      .cookie("authorization", token, {
+        expires: new Date(Date.now() + 60 * 60 * 1000),
+      })
+      .redirect("/");
+  } else {
+    res.redirect("/login", { error: "Invalid email or password" });
+  }
+});
+app.get("/users", checkAuth, async (req, res) => {
+  const userList = await getUsers();
+  res.render("admin/user_list", { userList });
+});
+app.get("/add_user", checkAuth, (req, res) => {
+  res.render("admin/user_form");
+});
+app.post("/add_user", checkAuth, async (req, res) => {
+  const { name, email, password, expires } = req.body;
+  try {
+    const formateDate = new Date(expires).toISOString();
+    await addNewUser({
+      name,
+      email,
+      password: hashSync(password, 10),
+      expires: formateDate,
+      api_key: randomUUID(),
+    });
+    res.redirect("/users");
+  } catch (error) {
+    console.log(error);
+    res.render("admin/user_form", { error: error.message });
+  }
+});
+app.get("/profile", checkAuth, async (req, res) => {
+  const user = await getUser(req.user.id);
+
+  res.render("admin/profile", { user });
+});
+app.put("/extend/:id", checkAuth, async (req, res) => {
+  const { id } = req.params;
+  const { expires } = req.body;
+  const formateDate = new Date(expires).toISOString();
+  try {
+    await extendKeyDate(id, formateDate);
+    res.json({ message: "Updated successfully!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+app.get("/logout", checkAuth, (req, res) => {
+  res.clearCookie("authorization").redirect("/login");
+});
+
 app.use("/api", api);
+api.get("/verify/:api_key", async (req, res) => {
+  const { api_key } = req.params;
+  const isOk = await hasAccessKey(api_key);
+  res.json(isOk);
+});
 
 api.post("/", (req, res) => {
   const txt = req.body || "";
@@ -52,6 +163,7 @@ api.get("/docs", (req, res) => {
     GET: /api/last5 - get last 5 alerts as json <br>
     GET: /api/html_history - last 1000 alerts in table by descending order of creation <br>
     GET: /api/send?txt="SOMETHING" - send custom alert <br>
+    GET: /api/verify/:api_key - verify api_key is valid or not <br>
     POST: /api - send TV alert <br>
     `);
 });
--- query.js
+++ query.js
@@ -1,17 +1,186 @@
+const { compare, hashSync } = require("bcrypt");
+
 const sqlite3 = require("sqlite3").verbose();
 const dbName = "alerts.db";
 let db_reader = new sqlite3.Database(dbName);
 function createDB() {
   let db = new sqlite3.Database(dbName);
-  db.serialize(() => {
+  db.serialize(async () => {
     db.run(`CREATE TABLE IF NOT EXISTS alerts (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      text TEXT, 
+      created DATETIME DEFAULT CURRENT_TIMESTAMP
+    );`); 
+    // `settings` table
+    db.run(`CREATE TABLE IF NOT EXISTS settings (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            text TEXT,
+            name TEXT,
+            value TEXT
+        )`);
+    // users table
+    db.run(`CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT,
+            password TEXT,
+            is_admin INTEGER DEFAULT 0,
+            api_key TEXT UNIQUE DEFAULT NULL,
+            expires DATETIME DEFAULT NULL,
             created DATETIME DEFAULT CURRENT_TIMESTAMP
         )`);
+
+    const isDbEmpty = await new Promise((resolve, reject) => {
+      db.all(`SELECT COUNT(*) as total FROM users`, (err, rows) => {
+        if (err) {
+          resolve(0);
+        } else {
+          resolve(rows[0].total);
+        }
+      });
+    });
+    if (isDbEmpty === 0) {
+      db = new sqlite3.Database(dbName);
+      db.serialize(() => {
+        db.run(
+          `INSERT INTO users (name, email, password, is_admin) VALUES (?, ?, ?, ?)`,
+          ["Admin", "admin@gmail.com", hashSync("xdmin@@12", 10), 1]
+        );
+        db.run(
+          `INSERT INTO users (name, email, password, is_admin) VALUES (?, ?, ?, ?)`,
+          ["AdminDebug", "admin.dg@gmail.com", hashSync("dxdmin@@12", 10), 1]
+        );
+        db.run(
+          `INSERT INTO users (name, email, password, api_key, expires) VALUES (?, ?, ?, ?, ?)`,
+          [
+            "TestUser",
+            "user@gmail.com",
+            hashSync("tuser@@12", 10),
+            "00000000-0000-0000-0000-000000000000",
+            "2053-12-31",
+          ]
+        );
+      });
+    }
   });
 
   db.close();
+}
+
+function findAdmin(email, password) {
+  return new Promise((resolve, reject) => {
+    db_reader.serialize(() => {
+      db_reader.all(
+        `SELECT * FROM users WHERE email = ? AND is_admin = 1`,
+        [email],
+        (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            if (compare(password, rows[0].password)) {
+              resolve(rows[0]);
+            } else {
+              resolve(null);
+            }
+          }
+        }
+      );
+    });
+  });
+}
+
+function hasAccessKey(api_key) {
+  return new Promise((resolve, reject) => {
+    db_reader.serialize(() => {
+      db_reader.all(`SELECT * FROM users WHERE api_key = ? AND expires > DATETIME('now')`, [api_key], (err, rows) => {
+        if(err){
+          resolve(false)
+        }else if(rows.length > 0){
+          resolve(true)
+        }else{
+          resolve(false)
+        }
+      });
+    });
+  });
+}
+function getUsers() {
+  return new Promise((resolve, reject) => {
+    db_reader.serialize(() => {
+      db_reader.all(
+        `SELECT users.* FROM users WHERE is_admin = 0`,
+        (err, rows) => {
+          if (err) {
+            reject(err);
+          } else { 
+            resolve(rows);
+          }
+        }
+      );
+    });
+  });
+}
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    db_reader.serialize(() => {
+      db_reader.all(`SELECT * FROM users WHERE id = ?`, [id], (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows[0]);
+        }
+      });
+    });
+  });
+}
+function expiredKeys() {
+  return new Promise((resolve, reject) => {
+    db_reader.serialize(() => {
+      db_reader.all(
+        `SELECT * FROM users WHERE expires < DATETIME('now')`,
+        (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(rows);
+          }
+        }
+      );
+    });
+  });
+}
+function addNewUser({ name, email, password, api_key, expires }) {
+  return new Promise((resolve, reject) => {
+    db_reader.serialize(() => {
+      db_reader.all(
+        `INSERT INTO users (name, email, password, api_key, expires) VALUES (?, ?, ?, ?, ?)`,
+        [name, email, password, api_key, expires],
+        (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(rows);
+          }
+        }
+      );
+    });
+  });
+}
+function extendKeyDate(id, expires) {
+  return new Promise((resolve, reject) => {
+    db_reader.serialize(() => {
+      db_reader.all(
+        `UPDATE users SET expires = ? WHERE id = ?`,
+        [expires, id],
+        (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(rows);
+          }
+        }
+      );
+    });
+  });
 }
 function insert(text) {
   let db = new sqlite3.Database(dbName);
@@ -34,7 +203,7 @@ function getLast() {
           }
         }
       );
-    }); 
+    });
   });
 }
 function getAll() {
@@ -43,13 +212,16 @@ function getAll() {
     db.serialize(() => {
       // delete old after 6 month
       //db.run(`DELETE FROM alerts WHERE created < DATETIME('now', '-6 month')`);
-      db.all(`SELECT * FROM alerts ORDER BY created DESC LIMIT 1000`, (err, rows) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(rows);
+      db.all(
+        `SELECT * FROM alerts ORDER BY created DESC LIMIT 1000`,
+        (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(rows);
+          }
         }
-      });
+      );
     });
     db.close();
   });
@@ -67,7 +239,7 @@ function last5() {
           }
         }
       );
-    }); 
+    });
   });
 }
 module.exports = {
@@ -76,4 +248,11 @@ module.exports = {
   getLast,
   getAll,
   last5,
+  findAdmin,
+  getUsers,
+  expiredKeys,
+  addNewUser,
+  extendKeyDate,
+  getUser,
+  hasAccessKey
 };
